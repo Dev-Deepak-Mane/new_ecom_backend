dd45ebaac1a04cbe6e1d97b0a8cf0b924ca68c87
Deepak Mane <thedpmane@gmail.com>
2023-07-23 13:48:42 +0530

    cookie

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,7 +1,8 @@
 // Create Token and saving in cookie
-const { serialize } = require("cookie");
+
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
+
   // options for cookie
   const options = {
     expires: new Date(
@@ -15,32 +16,6 @@ const sendToken = (user, statusCode, res) => {
     user,
     token,
   });
-  res.setHeader(
-    "Set-Cookie",
-    serialize("token", set ? token : "", {
-      path: "/",
-      httpOnly: true,
-      maxAge: set ? 15 * 24 * 60 * 60 * 1000 : 0,
-    })
-  );
 };
-export const cookieSeter = (res, user, set) => {
-  const token = user.getJWTToken();
-  res.setHeader(
-    "Set-Cookie",
-    serialize("token", set ? token : "", {
-      path: "/",
-      httpOnly: true,
-      maxAge: set ? 15 * 24 * 60 * 60 * 1000 : 0,
-    })
-  );
-};
-export const checkAuth = async (req) => {
-  const cookie = req.headers.cookie;
-  if (!cookie) return null;
-  const token = cookie.split("=")[1];
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  return await User.findById(decoded._id);
-};
 module.exports = sendToken;
