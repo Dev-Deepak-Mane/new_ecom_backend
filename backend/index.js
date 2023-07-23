48257d8d05c34a7756e714a6ccee195f04501340
Deepak Mane <thedpmane@gmail.com>
2023-07-23 14:04:07 +0530

    cookie

--- backend/index.js
+++ backend/index.js
@@ -16,23 +16,22 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 app.use(function (req, res, next) {
-  //res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", true);
-  // res.header(
-  //   "Access-Control-Allow-Headers",
-  //   "Origin, X-Requested-With, Content-Type, Accept"
-  // );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
-// app.use(
-//   cors({
-//   origin: "http://localhost:3000", // Update this with your frontend domain
-//   credentials: true, // Allow cookies to be sent in cross-origin requests
-// })
-// );
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
 
