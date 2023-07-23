58bc3f2e1ed34b79bcf7b1b6259f65e331947837
Dev-Deepak-Mane <140266501+Dev-Deepak-Mane@users.noreply.github.com>
2023-07-23 13:28:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 
 app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", req.headers.origin);
+   //res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", true);
-   res.header(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept"
-   );
+   // res.header(
+   //   "Access-Control-Allow-Headers",
+   //   "Origin, X-Requested-With, Content-Type, Accept"
+   // );
   next();
 });
 app.use(
