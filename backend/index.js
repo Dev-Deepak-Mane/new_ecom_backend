e702dfa4a171eab1e027a157e9f32de5645adae7
Dev-Deepak-Mane <140266501+Dev-Deepak-Mane@users.noreply.github.com>
2023-09-12 22:14:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
 });
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://dev-deepak-ecom.vercel.app/" || "http://localhost:3000",
   })
 );
 //app.use(cors({ origin: "*" }));
